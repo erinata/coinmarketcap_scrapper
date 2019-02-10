f4eaf7b13f23a24f1e9d1a66ac52ac58fbb0de26
Chungsang Tom Lam <erinata@gmail.com>
2019-02-10 15:59:55 -0500

    download coinmarketcap periodically

--- coinmarketcap_scrapper.py
+++ coinmarketcap_scrapper.py
@@ -1,15 +1,20 @@
 import urllib.request
 import os
-
-response = urllib.request.urlopen('https://coinmarketcap.com/')
-
-html = response.read()
-
-# print(html)
+import time
+import datetime
 
 if not os.path.exists("output"):
 	os.mkdir("output")
 
-f = open("output/coinmarketcap.html", "wb")
-f.write(html)
+for i in range(5):
+	current_time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S')
+	print(str(i) + ": " + current_time_stamp)
+	f = open("output/coinmarketcap" + current_time_stamp + ".html", "wb")
+	response = urllib.request.urlopen('https://coinmarketcap.com/')
+	html = response.read()
+	f.write(html)
+	f.close()
+	time.sleep(10)
+
+
 

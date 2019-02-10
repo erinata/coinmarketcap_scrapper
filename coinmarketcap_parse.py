45a44cea8690034e1afea348cff17b7ec2ff22e5
Chungsang Tom Lam <erinata@gmail.com>
2019-02-10 16:16:23 -0500

    use bs to parse one file

--- coinmarketcap_parse.py
+++ coinmarketcap_parse.py
@@ -1,2 +1,18 @@
+from bs4 import BeautifulSoup
+import os
+import glob
+
+if not os.path.exists("parsed_files"):
+	os.mkdir("parsed_files")
+
+
+
+f = open("html_files/coinmarketcap20190210160211.html", "r")
+
+# print(f.read())
+
+soup = BeautifulSoup(f.read(), 'html.parser')
+
+print(soup)
 
 

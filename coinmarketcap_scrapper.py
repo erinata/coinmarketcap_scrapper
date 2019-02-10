93ba0a832a91b6be23485ffd02ff854a713028d1
Chungsang Tom Lam <erinata@gmail.com>
2019-02-10 15:36:45 -0500

    write html to output folder

--- coinmarketcap_scrapper.py
+++ coinmarketcap_scrapper.py
@@ -1,8 +1,15 @@
 import urllib.request
+import os
 
 response = urllib.request.urlopen('https://coinmarketcap.com/')
 
 html = response.read()
 
-print(html)
+# print(html)
+
+if not os.path.exists("output"):
+	os.mkdir("output")
+
+f = open("output/coinmarketcap.html", "wb")
+f.write(html)
 

2a204967616449dacf9b57104babb6870b718842
Chungsang Tom Lam <erinata@gmail.com>
2019-02-10 17:03:56 -0500

    parse all rows

--- coinmarketcap_parse.py
+++ coinmarketcap_parse.py
@@ -21,20 +21,22 @@ currencies_tbody = currencies_table.find("tbody")
 currency_rows = currencies_tbody.find_all("tr")
 
 
-currency_short_name = currency_rows[0].find("td", {"class": "currency-name"}).find("span",{"class":"currency-symbol"}).find("a").text
-currency_name = currency_rows[0].find("td", {"class": "currency-name"}).find("a",{"class":"currency-name-container"}).text
-currency_market_cap = currency_rows[0].find("td", {"class": "market-cap"})['data-sort']
-currency_price = currency_rows[0].find("a",{"class": "price"}).text
-currency_volume = currency_rows[0].find("a",{"class": "volume"}).text
-currency_supply = currency_rows[0].find("td", {"class": "circulating-supply"})['data-sort']
-currency_change = currency_rows[0].find("td", {"class": "percent-change"})['data-sort']
 
-print(currency_short_name)
-print(currency_name)
-print(currency_market_cap)
-print(currency_price)
-print(currency_volume)
-print(currency_supply)
-print(currency_change)
+for r in currency_rows:
+	currency_short_name = r.find("td", {"class": "currency-name"}).find("span",{"class":"currency-symbol"}).find("a").text
+	currency_name = r.find("td", {"class": "currency-name"}).find("a",{"class":"currency-name-container"}).text
+	currency_market_cap = r.find("td", {"class": "market-cap"})['data-sort']
+	currency_price = r.find("a",{"class": "price"}).text
+	currency_volume = r.find("a",{"class": "volume"}).text
+	currency_supply = r.find("td", {"class": "circulating-supply"})['data-sort']
+	currency_change = r.find("td", {"class": "percent-change"})['data-sort']
+	print(currency_short_name)
+	print(currency_name)
+	print(currency_market_cap)
+	print(currency_price)
+	print(currency_volume)
+	print(currency_supply)
+	print(currency_change)
+	print("\n")
 
 

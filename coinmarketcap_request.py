1557d8b85b4e93b0c0976b4795bcdfde86d31bb6
Chungsang Tom Lam <erinata@gmail.com>
2019-02-10 16:00:59 -0500

    rename to request

--- coinmarketcap_request.py
+++ coinmarketcap_request.py
@@ -3,13 +3,13 @@ import os
 import time
 import datetime
 
-if not os.path.exists("output"):
-	os.mkdir("output")
+if not os.path.exists("html_files"):
+	os.mkdir("html_files")
 
 for i in range(5):
 	current_time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S')
 	print(str(i) + ": " + current_time_stamp)
-	f = open("output/coinmarketcap" + current_time_stamp + ".html", "wb")
+	f = open("html_files/coinmarketcap" + current_time_stamp + ".html", "wb")
 	response = urllib.request.urlopen('https://coinmarketcap.com/')
 	html = response.read()
 	f.write(html)
